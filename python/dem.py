0b107e18261737b0c4c7c8e6c813f2b7bc4ea246
shiva-1603 <109054628+shiva-1603@users.noreply.github.com>
2024-01-21 16:31:36 +0530

    Delete python directory

--- python/dem.py
+++
@@ -1,8 +0,0 @@
-def enterence(r=10,c=8):
-    if r in range(25) and c==8:
-        print("valid")
-    elif r in range(26,50) and c==8:
-        print("allowed")
-    else:
-        print("invalid")
-enterence()
--- python/lists.py
+++
@@ -1,3 +0,0 @@
-l=[1,2,3,4,5]
-for e in l:
-    print(e)
